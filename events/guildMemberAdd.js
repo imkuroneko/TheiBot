35df9bef3c0408c9700ada5fa40857e9df6194d5
KuroNeko <kuroinekowitch@gmail.com>
2022-05-26 10:29:30 -0400

    Fixeado para dar rol al unirse

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,28 +1,27 @@
 const channels = require('../config/channels.json');
-const config = require('../config/bot.json');
 const roles = require('../config/roles.json');
+const config = require('../config/bot.json');
 
 module.exports = {
     name: 'guildMemberAdd',
-    async execute(client, user) {
+    async execute(client) {
 
         if(client.user.id == config.clientId) { return; }
+
+        if(roles.roleWhenJoin.length > 0 ) {
+            client.roles.add(roles.roleWhenJoin);
+        }
 
         const userTag  = client.user.tag;
         const userId   = client.user.id;
         const username = client.user.username;
         const avatar   = client.user.displayAvatarURL();
 
-        if(roles.roleWhenJoin.length > 0 ) {
-            client.user.roles.add(roles.roleWhenJoin);
-        }
-
         if(channels.welcomeChannel.length > 0) {
             const sender_welcome = client.guild.channels.cache.get(channels.welcomeChannel);
             sender_welcome.send({ embeds: [{
                 color: 0xcc3366,
-                title: `Bienvenido <@${userId}> al servidor 👋🏻 Esperamos disfrutes tu estadía en el servidor.`,
-                description: "**Sobre Mí:** <#637941772063866890>\n**Reglas del servidor:** <#751891992178327573>\n**Obtén roles geniales:** <#938245623495393300>"
+                description: `Bienvenido <@${userId}> al servidor 👋🏻 Esperamos disfrutes tu estadía en el servidor.\n\n**Sobre Mí:** <#637941772063866890>\n**Reglas del servidor:** <#751891992178327573>\n**Obtén roles geniales:** <#938245623495393300>`
             }] });
         }
 
