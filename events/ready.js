e2d3df4419977933016a767eb04afbb8a3e69ba2
KuroNeko <kuroinekowitch@gmail.com>
2022-05-26 10:29:14 -0400

    Agregado update activity cada 60seg

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const channels = require('../config/channels.json');
+const activity = require('../config/activity.json');
 
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
@@ -8,12 +9,19 @@ module.exports = {
     name: 'ready',
     execute(guild) {
 
-        guild.user.setActivity('Hoomans en Discord™', {type: 'WATCHING'});
+        guild.user.setActivity("Hoomans en Discord™", { type: "WATCHING" });
+        if(activity.length > 0) {
+            var i = 0;
+            setInterval(() => {
+                i = (i + 1) % activity.length;
+                guild.user.setActivity(activity[i].message, { type: activity[i].type });
+            }, 60000);
+        }
 
         if(channels.presenceVoice.length > 0) {
             const { joinVoiceChannel } = require('@discordjs/voice');
             const voiceChannel = guild.channels.cache.get(channels.presenceVoice);
-        
+
             joinVoiceChannel({
                 channelId: voiceChannel.id,
                 guildId: voiceChannel.guild.id,
@@ -21,8 +29,7 @@ module.exports = {
                 selfDeaf: false
             });
         }
-    
-        console.log('[Init] ✨ Bot operativo!');
 
+        console.log('[Init] ✨ Bot operativo!');
     }
 };
