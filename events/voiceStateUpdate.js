76a7eef52a682ed0822702a7692babc3ffa26f5e
KuroNeko <kuroinekowitch@gmail.com>
2022-05-26 10:12:59 -0400

    Agregado para reconectar al bot a su voiceChannel

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,22 @@
+const channels = require('../config/channels.json');
+const config = require('../config/bot.json');
+
+module.exports = {
+    name: 'voiceStateUpdate',
+    async execute(oldState, newState) {
+
+        if((newState.channelID === null || typeof newState.channelID == 'undefined') && newState.id == config.clientId) {
+            if(channels.presenceVoice.length > 0) {
+                const { joinVoiceChannel } = require('@discordjs/voice');
+                const voiceChannelReconn = newState.guild.channels.cache.get(channels.presenceVoice);
+            
+                joinVoiceChannel({
+                    channelId: voiceChannelReconn.id,
+                    guildId: voiceChannelReconn.guild.id,
+                    adapterCreator: voiceChannelReconn.guild.voiceAdapterCreator,
+                    selfDeaf: false
+                });
+            }
+        }
+    }
+};
