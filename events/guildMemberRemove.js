a1e7980e704c7ee7d0604aa9e6d753e9b9786b03
KuroNeko <kuroinekowitch@gmail.com>
2022-07-16 12:59:29 -0400

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,14 +3,12 @@ const config = require('../config/bot.json');
 
 module.exports = {
     name: 'guildMemberRemove',
-    async execute(client, user) {
+    async execute(client) {
 
         if(client.user.id == config.clientId) { return; }
 
         var user     = client.user.tag;
         var userId   = client.user.id;
-        var username = client.user.username;
-        var avatar   = client.user.displayAvatarURL();
 
         if(channels.log_JoinLeft.length > 0) {
             const sender_log = client.guild.channels.cache.get(channels.log_JoinLeft);
@@ -18,8 +16,6 @@ module.exports = {
                 color: 0xe35d5d,
                 title: `👋🏻 Un usuario se acaba de ir del servidor`,
                 fields: [ { name: 'Usuario', value: user }, { name: 'User ID', value: userId } ],
-                // thumbnail: { url: avatar },
-                footer: { text: '🦄 Thei Bot / Experimental Project by KuroNeko' }
             }] });
         }
     }
