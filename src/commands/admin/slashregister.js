59fb6747623cba755b769a382fa43e156d5be7f8
KuroNeko <kuroinekowitch@gmail.com>
2026-05-05 23:58:34 -0300

    🩹 FIX comando slashregister

--- src/commands/admin/slashregister.js
+++ src/commands/admin/slashregister.js
@@ -12,13 +12,8 @@ exports.run = (client, message, args) => {
 
         const rest = new REST({ version: '10' }).setToken(token);
 
-        rest.put(Routes.applicationCommands(clientId), { body: client.slashRegister }).then((response) => {
-            rest.put(Routes.applicationGuildCommands(clientId, message.guild.id), { body: client.slashRegister }).then((response2) => {
-                message.reply(`🦄 Se registraron: ${response.length} comandos **de forma global** y ${response2.length} **en la guild**\n-# (Espera unos minutos hasta se reflejen los cambios)`);
-            }).catch((error) => {
-                message.reply(`\`[🦄 cmdPrefix:slashregister]\` ${error.message}`);
-                console.error('[cmdPrefix:slashregister]', error.message);
-            });
+        rest.put(Routes.applicationGuildCommands(clientId, message.guild.id), { body: client.slashRegister }).then((response) => {
+            message.reply(`🦄 Se registraron ${response.length} **en la guild**\n-# (Espera unos minutos hasta se reflejen los cambios)`);
         }).catch((error) => {
             message.reply(`\`[🦄 cmdPrefix:slashregister]\` ${error.message}`);
             console.error('[cmdPrefix:slashregister]', error.message);
